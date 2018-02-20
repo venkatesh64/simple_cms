003c1ed09946c1c9062d8bfa36f4b565744b4832
Taraka V Rao <vnakketla@gmail.com>
2018-02-19 21:36:04 -0600

    Associations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,7 @@
 class AdminUser < ApplicationRecord
 	#To configure a different table name
 	#self.table_name = "admin_users"
+	has_and_belongs_to_many :pages
+	has_many :section_edits
+	has_many :sections, :through => :section_edits
 end
