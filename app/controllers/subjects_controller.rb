f45b3c1ed0e6c801185909a96a44b4bbfc4910b4
Taraka V Rao <vnakketla@gmail.com>
2018-02-23 20:59:40 -0600

    Adding records to database with CRUD

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -13,9 +13,31 @@ class SubjectsController < ApplicationController
     @subject = Subject.new({:name => "Default"})
   end
 
+  def create
+    # Instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+    # Save the object
+    if @subject.save
+      # If save succeeds, redirect to the index action
+      redirect_to(:action => 'index')
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('new')
+    end
+  end
+
   def edit
   end
 
   def delete
   end
+
+  private
+
+    def subject_params
+      # same as using "params[:subject]", except that it:
+      # - raises an error if :subject is not present
+      # - allows listed attributes to be mass-assigned
+      params.require(:subject).permit(:name, :position, :visible)
+    end
 end
