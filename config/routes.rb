793de1e1f5e51bfaec27945eb0b26b30c5d6e1eb
Taraka V Rao <vnakketla@gmail.com>
2018-03-20 18:17:07 -0500

    Layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
-
-  get 'sections/index'
-
-  get 'sections/show'
-
-  get 'sections/new'
-
-  get 'sections/edit'
-
-  get 'sections/delete'
-
-	root "demo#index"
+  root "demo#index"
   #get 'demo/index'
   match ':controller(/:action(/:id))',:via => [:get, :post]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+	
